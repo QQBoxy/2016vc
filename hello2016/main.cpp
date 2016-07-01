d27e63e1a51dce73b88f7614f8d6a55ad3697c64
QQBoxy <gs1458@gmail.com>
2016-07-01 15:45:28 +0800

    Hello

---
+++ hello2016/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //引用函式庫，使用標準函式庫
+using namespace std; //標準函式庫的命名空間
+
+int main(void) //主函式寫法
+{
+	//送出Command命令: echo
+	system("echo Hello!");
+	//送出Command命令: pause
+	system("pause");
+	//回傳終止
+	return 0;
+}
